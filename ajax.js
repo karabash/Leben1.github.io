9f840a0288b0ca42a9e052f950a1124361969956
karabash <whyewre@gmail.com>
2016-03-03 10:28:46 +0100

    stars & vote fixed

--- ajax.js
+++ ajax.js
@@ -1,11 +1,51 @@
+
+
 //461907d1493ba089
 //Visa receptbetyg     GET https://edu.oscarb.se/sjk15/api/recipe/?api_key=461907d1493ba089&recipe=tiger_kaka
-
-
-
 
 //Rösta på ett recept   GET https://edu.oscarb.se/sjk15/api/recipe/?api_key=461907d1493ba089&recipe=tiger_kaka&rating=4
 
 
 
+// JavaScript (jQuery) för att hämta SMHI data (med AJAX)
+function showVotes() {
+	$("#messageDiv").html("<b>Hämtar röster... Var god vänta...</b>");
+	$.ajax({
+		method: "GET",
+		url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=461907d1493ba089&recipe=tiger_kaka",
+		success: function (data) {
+			$('#rating1').text(data.rating);
+			$('#vote').text(data.votes);
+			$('#recipe').text(data.recipe);
+			$("#messageDiv").empty();
+		},
+	});
+}
+
+
+$('.rating a').click(function () {
+	// Kod som körs när man klickar på knappen
+	console.log("dsada");
+	var rateValue = $(this).text();
+	var url = "https://edu.oscarb.se/sjk15/api/recipe/?api_key=461907d1493ba089&";
+	url = "https://edu.oscarb.se/sjk15/api/recipe/?api_key=461907d1493ba089&recipe=tiger_kaka&rating=4";
+	var params = "recipe=tiger_kaka&rating=" + rateValue;
+	params = "";
+	$("#messageDiv").html("<b>Skickar din röst... Var god vänta...</b>");
+
+	$.ajax({
+		method: "GET",
+		data: { user_rating: rateValue },
+		url: url + params,
+		success: function (data) {
+			showVotes();
+			$("#messageDiv").empty();
+		}
+	});
+
+});
+
+$(document).ready( function(){
+	showVotes();
+});
 
