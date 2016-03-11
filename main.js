d1392712d031ea435b9f3c46c49e1f79116e02e4
karabash <whyewre@gmail.com>
2016-03-11 15:30:56 +0100

    Jesper has checked

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 
-var totalperson =1;
+var totalperson = 1;
 var first = 200;
-var second = 1;    
+var second = 1;
 var thirt = 3;
 var fourth = 4;
 var fifth = 1;
@@ -10,100 +10,79 @@ var sixth = 1;
 var seventh = 4;
 var eighth = 2;
 
-
-
-
-
 function clickedButton() {
-     totalperson =totalperson+1;
-    if(totalperson<=100){
-    first = first + 200; 
-    second = second + 1; 
-    thirt = thirt + 3;
-    fourth = fourth + 4;
-    fifth = fifth +1;
-	sixth = sixth +1;
-    seventh = seventh+4;
-      eighth = eighth +2;
-        
-    // För att hämta en HTML-tagg i vår HTML-fil...
-    var tP = document.getElementById("personsT");
-    var f = document.getElementById("1");
-    var s = document.getElementById("2");
-    var t = document.getElementById("3");
-    var fourth1 = document.getElementById("4");
-	var fifth1 = document.getElementById("5");
-	var sixth1 = document.getElementById("6");
-	var seventh1 = document.getElementById("7");
-var eighth1 = document.getElementById("8");
-     
-		
-   
+	totalperson = totalperson + 1;
+	if (totalperson <= 100) {
+		first = first + 200;
+		second = second + 1;
+		thirt = thirt + 3;
+		fourth = fourth + 4;
+		fifth = fifth + 1;
+		sixth = sixth + 1;
+		seventh = seventh + 4;
+		eighth = eighth + 2;
 
-    
-    
-    // ...och ändra på innehållet mellan start- och slut-tagg:
-   tP.innerHTML = totalperson;
-    f.innerHTML = first;
-    s.innerHTML = second;
-    t.innerHTML = thirt;
-    fourth1.innerHTML = fourth;
-    fifth1.innerHTML = fifth;
-	sixth1.innerHTML = sixth;
-	seventh1.innerHTML =seventh;
+		// För att hämta en HTML-tagg i vår HTML-fil...
+		var tP = document.getElementById("personsT");
+		var f = document.getElementById("1");
+		var s = document.getElementById("2");
+		var t = document.getElementById("3");
+		var fourth1 = document.getElementById("4");
+		var fifth1 = document.getElementById("5");
+		var sixth1 = document.getElementById("6");
+		var seventh1 = document.getElementById("7");
+		var eighth1 = document.getElementById("8");
+
+
+		// ...och ändra på innehållet mellan start- och slut-tagg:
+		tP.innerHTML = totalperson;
+		f.innerHTML = first;
+		s.innerHTML = second;
+		t.innerHTML = thirt;
+		fourth1.innerHTML = fourth;
+		fifth1.innerHTML = fifth;
+		sixth1.innerHTML = sixth;
+		seventh1.innerHTML = seventh;
 		eighth1.innerHTML = eighth;
-    }
+	}
 
 }
 
 function clickedButton1() {
-     if(totalperson>1){
-    
-    first = first - 200; 
-    second = second - 1; 
-    thirt = thirt - 3;
-    totalperson =totalperson - 1;
-    fourth = fourth - 4;
-    fifth = fifth - 1;
-	sixth = sixth -1;
-		 seventh = seventh-4;
-  eighth = eighth - 2;
-        
-		 
+	if (totalperson > 1) {
 
-         
-    
-        var tP = document.getElementById("personsT");
-        var f = document.getElementById("1");
-        var s = document.getElementById("2");
-        var t = document.getElementById("3");
-        var fourth1 = document.getElementById("4");
-        var fifth1 = document.getElementById("5");
+		first = first - 200;
+		second = second - 1;
+		thirt = thirt - 3;
+		totalperson = totalperson - 1;
+		fourth = fourth - 4;
+		fifth = fifth - 1;
+		sixth = sixth - 1;
+		seventh = seventh - 4;
+		eighth = eighth - 2;
+
+
+		var tP = document.getElementById("personsT");
+		var f = document.getElementById("1");
+		var s = document.getElementById("2");
+		var t = document.getElementById("3");
+		var fourth1 = document.getElementById("4");
+		var fifth1 = document.getElementById("5");
 		var sixth1 = document.getElementById("6");
-	    var seventh1 = document.getElementById("7");
-        var eighth1 = document.getElementById("8");
+		var seventh1 = document.getElementById("7");
+		var eighth1 = document.getElementById("8");
 
-       
-		 
+		tP.innerHTML = totalperson;
+		f.innerHTML = first;
+		s.innerHTML = second;
+		t.innerHTML = thirt;
+		fourth1.innerHTML = fourth;
+		fifth1.innerHTML = fifth;
+		sixth1.innerHTML = sixth;
+		seventh1.innerHTML = seventh;
+		eighth1.innerHTML = eighth;
 
-
-
-   tP.innerHTML = totalperson;
-    f.innerHTML = first;
-    s.innerHTML = second;
-    t.innerHTML = thirt;
-    fourth1.innerHTML = fourth;
-    fifth1.innerHTML = fifth;
-    sixth1.innerHTML = sixth;
-    seventh1.innerHTML =seventh;
-		 		eighth1.innerHTML = eighth;
-
-
-		 
-		 
-
-         
-     }
+	}
 
 }
 
--- stars.js
+++ stars.js
@@ -1,31 +1,65 @@
-jQuery(function() {
+//$('.rating a').click(function () {
+//	var star = $(this);
+//	star.addClass('checked');
+//	return false;
+//});
+var currentSelectedStarCount = 0;
 
-var rating = {
-	init: function() {
-  	$('.rating').on('mouseover mousedown', '.star', this.doEvent);
-  },
-  
-	doEvent: function(event) {
-  	var srcEl = $(this),
-    		rate;
-    
-  	switch(event.type) {
-    	case 'mouseover':
-      	// turn off all stars
-      	srcEl.parent().find('.active').removeClass('active');
-        // turn on only affected stars
-      	srcEl.addClass('active')
-        	.prevAll('.star').addClass('active');
-      	break;
-    	case 'mousedown':
-      	// since elements starts counting with 'zero' as first
-      	rate = srcEl.index() + 1;
-      	alert(rate);
-      	break;
-    }
-  }
-};
+$('.rating a').hover(function ()
+{
+	console.log("hover");
+	var star = $(this);
+	var checkedStarsCount = $(".checked-star").length;
+	var currentStarNr = star.text();
+	console.log(currentStarNr + " " + currentStarNr);
 
-rating.init();
+	if (!star.hasClass("checked-star") && (currentSelectedStarCount == 0 || currentStarNr > currentSelectedStarCount))
+	{
+		var previousStars = $(star.parent().prevAll("li").find(".unchecked-star"));
+		console.log("has not " + previousStars.length);
+		previousStars.addClass("checked-star");
+		previousStars.removeClass("unchecked-star");
+		star.addClass("checked-star");
+		star.removeClass("unchecked-star");
+	}
+});
 
+$('.rating a').mouseout(function () {
+	console.log("mouseout");
+	var star = $(this);
+	var checkedStarsCount = $(".checked-star").length;
+	var currentStarNr = star.text();
+	console.log(currentStarNr + " " + currentStarNr);
+
+	if (star.hasClass("checked-star") && (currentSelectedStarCount == 0))
+	{
+		var previousStars = $(star.parent().prevAll("li").find(".checked-star"));
+		previousStars.removeClass("checked-star");
+		previousStars.addClass("unchecked-star");
+		star.removeClass("checked-star");
+		star.addClass("unchecked-star");
+	}
+	else
+	{
+		if (currentStarNr > currentSelectedStarCount) {
+			star.removeClass("checked-star");
+			star.addClass("unchecked-star");
+		}
+	}
+	return false;
+});
+
+$('.rating a').click(function ()
+{
+	var star = $(this);
+	$(".checked-star").addClass("unchecked-star");
+	$(".checked-star").removeClass("checked-star");
+	var previousStars = $(star.parent().prevAll("li").find(".unchecked-star"));
+	previousStars.removeClass("unchecked-star");
+	previousStars.addClass("checked-star");
+	star.addClass("checked-star");
+	star.removeClass("unchecked-star");
+	currentSelectedStarCount = $(".checked-star").length;
+	$("#debug").text(currentSelectedStarCount);
+	return false;
 });
